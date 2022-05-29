17678ded346767256cbf9ef32b23ac55cf153460
MuhammadAakash <muhammadaakash249@gmail.com>
2022-05-29 11:22:49 +0500

    Updated Home Page.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
-
+import {Link} from 'react-router-dom';
 
 function Home(){
     return(
         <div>
-            <h2>This is a Home Page</h2>
+            <h2 className="center">Welcome to the Home Page.</h2>
+            <h3 className="center"> Go to Products page to see all <Link to="/">Products</Link>
+            
+            </h3>
         </div>
     )
 }
